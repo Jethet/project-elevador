092994cee3c9df77133f8d17ffbe72b3f045c697
Jethet <henriette.hettinga@gmail.com>
2019-09-25 13:54:36 +0200

    Create views for 'about', 'gallery', 'who' and 'contact'

--- placeholder/views.py
+++ placeholder/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def who(request):
+    return render(request, 'who.html')
+
+def contact(request):
+    return render(request, 'contact.html')
