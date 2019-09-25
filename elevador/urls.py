28164042681b761f7889dbdfbee1cb28edf4a9ef
Jethet <henriette.hettinga@gmail.com>
2019-09-25 14:18:26 +0200

    Debug urls and views

--- elevador/urls.py
+++ elevador/urls.py
@@ -19,5 +19,9 @@ from placeholder import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('who/', views.who, name='who'),
+    path('contact/', views.contact, name='contact'),
 ]
