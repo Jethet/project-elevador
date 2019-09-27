46e4a1a3f2a1a91f9fb75be7a76bfbb22900dc95
Jethet <henriette.hettinga@gmail.com>
2019-09-27 15:36:21 +0200

    Add 'home' nav and combine 'about' and 'who' navs

--- elevador/urls.py
+++ elevador/urls.py
@@ -19,6 +19,7 @@ from placeholder import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('gallery/', views.gallery, name='gallery'),
