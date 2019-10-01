a82ab7e25c5c116f42434518de8904a0be175e2f
Jethet <henriette.hettinga@gmail.com>
2019-10-01 11:34:51 +0200

    Add allowed hosts (herokuapp plus domainname elevador.es)

--- elevador/settings.py
+++ elevador/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hhvzs)(ssp$(+f@ru1%j#*$#(^ed96wtie496&12-mb9$ul@_w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thawing-refuge-46763.herokuapp.com', 'elevador.es', 'www.elevador.es']
 # ADDED THE FOLLOWING TO GET OUT OF SERVER ERROR:
 SECURE_SSL_REDIRECT = False
 
